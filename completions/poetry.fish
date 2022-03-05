23953b428a67abd46f3eb888ee926ca094345f7c
Rodrigo Cristiano <rcristianofv@hotmail.com>
2022-03-04 23:08:06 -0300

    ✨ add env remove completions with available envs

--- completions/poetry.fish
+++ completions/poetry.fish
@@ -7,6 +7,16 @@ function __pua_seen_subcmds
     return 1
 end
 
+function __pua_has_envs
+    set -l envs (poetry env list)
+    if test $status -eq 0
+        return 0
+    end
+    return 1
+end
+
+# env
+
 set -l poetry_env_subcommand "info list remove use"
 complete -f -c poetry -n "__fish_seen_subcommand_from env; and not __fish_seen_subcommand_from $poetry_env_subcommand" -a "info" -d "Displays information about the current environment."
 complete -f -c poetry -n "__fish_seen_subcommand_from env; and not __fish_seen_subcommand_from $poetry_env_subcommand" -a "list" -d "Lists all virtualenvs associated with the current project."
@@ -17,3 +27,7 @@ complete -f -c poetry -n "__fish_seen_subcommand_from env; and not __fish_seen_s
 
 set -l python_versions (string trim (asdf list python))
 complete -f -c poetry -n "__pua_seen_subcmds env use" -a "$python_versions"
+
+# env remove
+
+complete -f -c poetry -n "__pua_seen_subcmds env remove; and __pua_has_envs" -a "(string replace ' (Activated)' '' (poetry env list))"
