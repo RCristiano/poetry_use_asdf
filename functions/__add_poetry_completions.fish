ad78097f3ecfb03780421cf824571079c0b4b100
Rodrigo Cristiano <rcristianofv@hotmail.com>
2022-03-04 23:14:10 -0300

    💡 Mark the function as too slow

--- functions/__add_poetry_completions.fish
+++ functions/__add_poetry_completions.fish
@@ -1,3 +1,5 @@
+# Very slow
+
 function __add_poetry_completions --description "Adds "poetry" sub-commands completions based on autodetection with regex from poetry help <command>"
     set sub_commands (string trim (string unescape -- (string match -r '\'\s{2}\w*\''  (string escape -- (poetry env --help)))))
 
